a2234e844a427f57341f00a082e7f89b623dc3b3
alif03122000 <u1904050@student.cuet.ac.bd>
2024-05-29 00:30:42 +0600

    Secpnd commit

--- Main.java
+++ Main.java
@@ -1,41 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
-
         System.out.println("Hello and welcome!");
 
-        System.out.printf("Initializing banking system..");
+        System.out.printf("Initializing banking system...\n");
 
-        int totalNumberOfSimulaion = 10;
+        int totalNumberOfSimulation = 10;
         OperationsQueue operationsQueue = new OperationsQueue();
         Bank bank = new Bank("123", operationsQueue);
 
-        System.out.println("Initializing simulation....");
+        System.out.println("Initializing simulation...");
         Thread simulationThread = new Thread(() -> {
-            operationsQueue.addSimulation(totalNumberOfSimulaion);
+            operationsQueue.addSimulation(totalNumberOfSimulation);
         });
         simulationThread.start();
 
-
-        System.out.printf("Initializing deposit systen....");
+        System.out.printf("Initializing deposit system...\n");
         Thread depositThread = new Thread(() -> {
             bank.deposit();
         });
         depositThread.start();
-        System.out.println("coompleted");
 
-        System.out.printf("Initializing withdraw systen....");
+        System.out.printf("Initializing withdraw system...\n");
         Thread withdrawThread = new Thread(() -> {
             bank.withdraw();
         });
         withdrawThread.start();
-        System.out.println("coompleted");
-
-
-        System.out.println("coompleted");
-
     }
 }
--- OperationsQueue.java
+++ OperationsQueue.java
@@ -1,16 +1,17 @@
 import java.util.List;
-import java.util.ArrayList;;
+import java.util.ArrayList;
+
 public class OperationsQueue {
     private final List<Integer> operations = new ArrayList<>();
 
     public void addSimulation(int totalSimulation) {
-
         // Add 50 random numbers in the operations list. The number will be range from -100 to 100. It cannot be zero.
         for (int i = 0; i < totalSimulation; i++) {
-            int random = (int) (Math.random() * 200) - 100;
-            if (random != 0) {
-                operations.add(random);
-            }
+            int random;
+            do {
+                random = (int) (Math.random() * 200) - 100;
+            } while (random == 0);
+            operations.add(random);
             System.out.println(i + ". New operation added: " + random);
             // add small delay to simulate the time taken for a new customer to arrive
             try {
@@ -19,17 +20,19 @@ public class OperationsQueue {
                 e.printStackTrace();
             }
         }
-        operations.add(-9999);
+        operations.add(-9999); // Sentinel value to signal the end of the operations
     }
-    public void add(int amount) {
+
+    public synchronized void add(int amount) {
         operations.add(amount);
+        notifyAll(); // Notify waiting threads that a new operation has been added
     }
 
     public synchronized int getNextItem() {
         // add a small delay to simulate the time taken to get the next operation.
-        while(operations.isEmpty()) {
+        while (operations.isEmpty()) {
             try {
-                Thread.sleep(100);
+                wait(); // Wait until an operation is available
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
